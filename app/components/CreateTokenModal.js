ba5e2e510a519d95e3e009a63c2e92edfe404e86
Chokkalingam.S <lingam1707@gmail.com>
2025-02-10 16:22:12 +0530

    paired token address update (mainnet)

--- app/components/CreateTokenModal.js
+++ app/components/CreateTokenModal.js
@@ -29,7 +29,7 @@ export function CreateTokenModal() {
     tokenSymbol: "T",
     fee: "3000",
     salt: "randomSalt",
-    pairedToken: "0xF5561b9cE91092f60323a54Dd21Dd66F8f0A9279",
+    pairedToken: "0x9d6501275e91c0b2b0845c2c5334dea1ec6a3c18",
     fid: 122,
     castHash: "hash",
     earthToken: "",
